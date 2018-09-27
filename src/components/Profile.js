673cd0006ef3cee6d6f43718b4f887be53ce7124
AndrewBantug <AndrewBantug@gmail.com>
2018-09-27 10:29:03 -0400

    profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <img src="http://www.placecage.com/201/370" alt="nikki" />
+        <h1>Nikki Cage</h1>
+        <h3>Full Stack Developer</h3>
+      </div>
+    )
+  }
+}
+
+export default Profile
