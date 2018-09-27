ef543189b1f55a6c3e6f86c6be9f13133df9a29e
AndrewBantug <AndrewBantug@gmail.com>
2018-09-27 10:48:20 -0400

    specialties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Profile from './components/Profile';
+import Specialties from './components/Specialties';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Profile />
+        <Specialties />
       </div>
     );
   }
